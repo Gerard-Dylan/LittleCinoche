2349875f10fad65b0da63e24258eb8180cc99bb7
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-04-08 12:16:21 +0200

    final change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,12 +6,14 @@ type NavBarProps = {
 
 function NavBar ({setFilter}: NavBarProps) {
     return (
+
         <nav className="Nav">
             <button onClick={() => setFilter("All")}>LittleCinoch</button>
             <button onClick={() => setFilter("Science-Fiction")}>Science-Fiction</button>
             <button onClick={() => setFilter("Comédie")}>Comédie</button>
             <button onClick={() => setFilter("favoris")}>Favoris</button>
         </nav>
+
     )
 } 
 
